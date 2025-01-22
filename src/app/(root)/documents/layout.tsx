d7a58da0b5f8b27bf900637df4c88ab3552bf33c
David Bobe <bobe.david@live.com>
2025-01-22 12:48:04 -0500

    documents page route created

---
+++ src/app/(root)/documents/layout.tsx
@@ -0,0 +1,18 @@
+import CustomerList from "@/components/CustomerList";
+import { ReactNode } from "react";
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <div className="flex flex-1 flex-col size-full">
+      <div className="px-2 w-full h-min bg-sidebar text-primary-foreground">
+        <h1 className="text-lg">Document Management</h1>
+      </div>
+      <div className="flex flex-1 overflow-hidden">
+        <CustomerList />
+        <div className="flex-1 overflow-auto flex flex-col justify-between">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/(root)/documents/page.tsx
@@ -0,0 +1,7 @@
+export default function DocumentsPage() {
+  return (
+    <div className="flex-1 overflow-auto flex flex-col justify-between">
+      Documents
+    </div>
+  );
+}
